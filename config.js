b1c57cb6c4c4e0bea966adbdfad68ba710964240
lascalonetacyberpubli-crypto <lascalonetacyberpubli@gmail.com>
2026-02-05 18:01:02 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 // Archivo de configuración para el número de línea de WhatsApp
 export const NUMERO_DE_LINEA = '595992022127';
 // Configuración de Meta Pixel
-export const META_PIXEL_ID = '1276678713442753';
+export const META_PIXEL_ID = '579805081806733';
 
 export const META_EVENT_NAME = 'ClickFortu';
+
 
