ed434fdae779674dd85b3319b1070dd8501e321a
lascalonetacyberpubli-crypto <lascalonetacyberpubli@gmail.com>
2026-02-04 22:48:25 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// Archivo de configuración para el número de línea de WhatsApp
+export const NUMERO_DE_LINEA = '5491176116901';
+// Configuración de Meta Pixel
+export const META_PIXEL_ID = '1276678713442753';
+export const META_EVENT_NAME = 'ClickTrebol';
